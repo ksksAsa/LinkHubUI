8beab5467a00e8f8489259f73dda524a632118ff
ksksAsa <skakaes@hotmail.com>
2017-01-08 15:07:10 +0200

    First commit

---
+++ LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BOL;
+
+namespace LinkHubUI.Areas.User.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: User/URL
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        public ActionResult Create(tbl_Url objUrl)
+        {
+            return View();
+        }
+    }
+}
